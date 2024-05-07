d2f81bc5bcf1805cb70a4612d9de524f47295cd9
hienvl125 <hienviluong125@gmail.com>
2024-05-07 13:02:13 +0700

    feat: implement custom view for login page
    - Use 'unauth' layout for sign_in/sign_up/forgot_password pages
    - Implement flash message with close button and auto hide after 5 seconds

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,23 @@
 class ApplicationController < ActionController::Base
+  layout :determine_layout
+
+  private 
+
+  def determine_layout
+    if !user_signed_in? && sign_in_or_sign_up_or_forgot_password_action?
+      "unauth"
+    else
+      "application"
+    end
+  end
+
+  def sign_in_or_sign_up_or_forgot_password_action?
+    return true if params[:controller] == "devise/sessions" && params[:action] == "new"
+
+    return true if params[:controller] == "users/registrations" && params[:action] == "new"
+
+    return true if params[:controller] == "devise/passwords" && params[:action] == "new"
+
+    return false 
+  end
 end
